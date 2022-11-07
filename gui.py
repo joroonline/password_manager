b9cc05e8788115e4db01cde7542e69f00da00b9d
joroonline <jo-ro-online@web.de>
2022-11-07 20:03:30 +0100

    Initial commit

---
+++ gui.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setGeometry(300, 300, 600, 400)
+        self.setWindowTitle("Password Manager")
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = Window()
+    sys.exit(app.exec())
